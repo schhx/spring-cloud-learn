624bf701cd67f614befba3082210e4a70c864a0a
shanchao <shanchao@wosai-inc.com>
2018-05-24 10:44:26 +0800

    升级版本到Edgware.SR3

--- eureka-consumer-feign-hystrix/src/main/java/org/schhx/springbootlearn/EurekaConsumerFeignHystrixApplication.java
+++ eureka-consumer-feign-hystrix/src/main/java/org/schhx/springbootlearn/EurekaConsumerFeignHystrixApplication.java
@@ -2,11 +2,9 @@ package org.schhx.springbootlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaConsumerFeignHystrixApplication {
 
